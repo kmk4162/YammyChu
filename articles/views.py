37585c8adb44dcd6be8408267d0b9103760d9351
kmk4162 <kmk41623@gmail.com>
2022-11-10 01:58:40 +0900

    articles url 연결 및 base.html 색깔만 추가

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+
+    return render(request, 'articles/index.html')
+
+def create(request):
+    
+    return render(request, 'articles/create.html')
+
+def update(request):
+
+    return render(request, 'articles/update.html')
+
+def delete(request):
+
+    return redirect('articles:index')
+
+def comments_create(request):
+
+    return redirect('articles:index')
+
+def like(request):
+
+    return redirect('articles:index')
+
+def detail(request):
+
+    return render(request, 'articles/detail.html')
