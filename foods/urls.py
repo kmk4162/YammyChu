d5f17d7b81b9abcb3c4d33acddd9c34388c72b4b
kmk4162 <kmk41623@gmail.com>
2022-11-14 10:00:05 +0900

    fix: 자잘한 수정한다

--- foods/urls.py
+++ foods/urls.py
@@ -5,7 +5,7 @@ app_name = "foods"
 
 urlpatterns = [
     path("home/<int:team_pk>/", views.home, name="home"),
-    path('<int:store_pk>/detail', views.detail, name='detail'),
-    path('<int:store_pk>/create', views.create, name='create'),
-    path('<int:store_pk>/<int:review_pk>/delete', views.delete, name='delete'),
+    path('home/<int:team_pk>/<int:store_pk>/', views.detail, name='detail'),
+    path('home/<int:team_pk>/<int:store_pk>/create/', views.create, name='create'),
+    path('home/<int:team_pk>/<int:store_pk>/<int:review_pk>/delete/', views.delete, name='delete'),
 ]
