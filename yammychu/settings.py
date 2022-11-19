88b56308525d001107154bf775378547ca7f95c0
pangpyo <kkp0639@gmail.com>
2022-11-20 00:12:22 +0900

    fix : deplay 수정한다

--- yammychu/settings.py
+++ yammychu/settings.py
@@ -171,8 +171,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 AUTH_USER_MODEL = "accounts.User"
+
 # MEDIA_ROOT = BASE_DIR / 'media'
 # MEDIA_URL = '/media/'
+
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
